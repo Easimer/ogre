88601c09757981ef4fb9c3e39087bdf72a7d80a1
Heilig Benedek <benecene@gmail.com>
2021-01-11 20:25:30 +0100

    ANGLE: make it easier to build with electron ANGLE

--- CMake/ConfigureBuild.cmake
+++ CMake/ConfigureBuild.cmake
@@ -200,7 +200,7 @@ if (UNIX)
     configure_file(${OGRE_TEMPLATES_DIR}/OGRE-MeshLodGenerator.pc.in ${PROJECT_BINARY_DIR}/pkgconfig/OGRE-MeshLodGenerator.pc @ONLY)
     install(FILES ${PROJECT_BINARY_DIR}/pkgconfig/OGRE-MeshLodGenerator.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
   endif ()
-  
+
   if (OGRE_BUILD_COMPONENT_TERRAIN)
     if (OGRE_BUILD_COMPONENT_PAGING)
       set(OGRE_PAGING_ADDITIONAL_PACKAGES ", OGRE-Paging = ${OGRE_VERSION}")
