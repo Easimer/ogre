6cda63b0f03543b1ec147fabe544e439a13f3984
Dániel Mészáros <daniel.meszaros@maxwhere.com>
2023-12-09 22:01:48 +0100

    GLES2: turn on normalization for VET_INT_10_10_10_2_NORM

--- RenderSystems/GLES2/src/OgreGLES2RenderSystem.cpp
+++ RenderSystems/GLES2/src/OgreGLES2RenderSystem.cpp
@@ -1700,6 +1700,7 @@ namespace Ogre {
             case VET_USHORT2_NORM:
             case VET_SHORT4_NORM:
             case VET_USHORT4_NORM:
+            case VET_INT_10_10_10_2_NORM:
                 normalised = GL_TRUE;
                 break;
             default:
